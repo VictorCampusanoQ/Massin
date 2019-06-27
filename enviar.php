7b68a98fbeb0dafd509064d49c96b3e3d7802813
Victorius <victor.campusano.q@gmail.com>
2019-06-27 16:41:30 -0400

    primer commit del sitio de massin

--- enviar.php
+++ enviar.php
@@ -1,11 +1,32 @@
 <?php  
+if(!empty($_POST)){
+	// Llamando a los campos
+	$nombre = $_POST['nombre'];
+	$correo = $_POST['correo'];
+	$telefono = $_POST['telefono'];
+	$mensaje = $_POST['mensaje'];
+	$captcha = $_POST['g-recaptcha-response'];
+		
+		$secret = '6Lc8DKsUAAAAAM9YlD4YE_ZRt6iVBVP53Cuw-APi';
+		
+		if(!$captcha){
 
-// Llamando a los campos
-$nombre = $_POST['nombre'];
-$correo = $_POST['correo'];
-$telefono = $_POST['telefono'];
-$mensaje = $_POST['mensaje'];
-
+			echo "Por favor verifica el captcha";
+			
+			} else {
+			
+			$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify");
+			
+			$arr = json_decode($response, TRUE);
+			
+			if($arr['success'])
+			{
+				echo '<h2>Gracias</h2>';
+				} else {
+				echo '<h3>Error al comprobar Captcha </h3>';
+			}
+		}
+}
 // Datos para el correo
 $destinatario = "contacto@massin.cl";
 $asunto = "Contacto desde nuestra web";
@@ -20,3 +41,5 @@ mail($destinatario, $asunto, $carta);
 header('Location:contacto.html');
 
 ?>
+
+
